ba0fe9478d9c6fadf5f7d0e0ce0ad141b9b3f112
Paul Weaver <paul@concertdaw.co.uk>
2017-09-28 09:13:25 +0100

    Add in github redirects

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,13 +4,17 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Applicative ((<|>))
+import Data.ByteString as BS
 import Data.ByteString.Lazy.UTF8 (fromString)
 import Data.Monoid ((<>))
-import qualified Network.Wai as Wai
-import Network.Wai.Handler.Warp (run)
 import qualified Network.HTTP.Types as HTTP
 import Network.HTTP.Types.Header (RequestHeaders, hContentType, hLocation)
+import qualified Network.Wai as Wai
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.UrlMap (mapUrls, mount, mountRoot)
 
+-- Secure-only redirect middleware:
 hasHttpsHeader :: RequestHeaders -> Bool
 hasHttpsHeader hs = maybe False (== "https") $ lookup "X-Forwarded-Proto" hs
 
@@ -34,12 +38,35 @@ forceTls app req sendResponse =
     location host =
       "https://" <> host <> Wai.rawPathInfo req <> Wai.rawQueryString req
 
-app :: Wai.Application
-app req sendResponse =
+
+-- Redirect sub-application
+
+githubRedir :: BS.ByteString -> Wai.Application
+githubRedir user req sendResponse =
+    -- FIXME: only support GET?
+    sendResponse $ Wai.responseBuilder
+      HTTP.status307
+      [(hLocation, "https://github.com/" <> user)]
+      mempty
+
+
+-- Catch-all sub-application
+
+defaultApp :: Wai.Application
+defaultApp req sendResponse =
     sendResponse $ Wai.responseLBS
       HTTP.status200
       [(hContentType, "text/plain")]
       (fromString . show $ Wai.requestHeaders req)
+
+
+-- Main application
+
+app :: Wai.Application
+app = mapUrls $
+    mount "david" (githubRedir "foolswood")
+    <|> mount "paul" (githubRedir "ch3pjw")
+    <|> mountRoot defaultApp
 
 someFunc :: Int -> IO ()
 someFunc port = run port $ forceTls app
