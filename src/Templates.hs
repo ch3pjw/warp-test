a49d919ecb08da8c0a13f5499c17ca9982ff4c67
Paul Weaver <paul@concertdaw.co.uk>
2017-10-23 09:10:20 +0100

    Change HTML template for email submission page to have actual pretty content

--- src/Templates.hs
+++ src/Templates.hs
@@ -2,21 +2,80 @@
 
 module Templates where
 
+import Prelude hiding (div)
+import Control.Monad
 import Data.Text (Text)
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Internal (customParent, MarkupM)
+
+id_ :: AttributeValue -> Attribute
+id_ = A.id
+
+-- FIXME: this should be injected into the page
+mainSiteLink = "https://www.concertdaw.co.uk"
 
 emailSubmission :: Html
 emailSubmission = docTypeHtml $ do
   H.head $ do
+    meta ! name "viewport" ! content "width=device-width, initial-scale=1"
     H.title "Register Interest - Concert"
+    link ! rel "stylesheet" ! href "/screen.css"
   body $ do
-    H.form ! method "post" $ do
-      "Email address:"
-      br
-      input ! type_ "text" ! name "email"
-      br
-      input ! type_ "submit" ! value "Go!"
+    header ! id_ "header-wrapper" $ do
+      div ! id_ "header" $ do
+        a ! href mainSiteLink $ do
+          picture $ do
+            source
+            img
+        a ! href "/about" $ "About Us"
+
+    div ! id_ "content-wrapper" $ do
+      div ! id_ "content" $ do
+        div ! id_ "description" $ do
+          h1 $ do
+            "Collaborative Audio Production"
+          p $ do
+            "We're building tools to help you work on audio projects together "
+            "across the internet."
+          p $ do
+            "Our software is "
+            a ! href "https://github.com/concert" $ do
+              "open source"
+            " wherever possible so that you are free to use it however you "
+            "like and you'll always be able to access your data."
+          p $ do
+            "Sign up to our pre-release mailing list to register interest in "
+            "beta testing."
+        H.form ! method "post" ! id_ "registration-form" $ do
+          H.label ! for "email" $ do
+            "Email"
+          input ! id_ "email" ! type_ "text" ! placeholder "name@example.com"
+            ! autofocus "true" -- FIXME: is this right?
+          input ! type_ "submit" ! value "Register interest"
+          aside $ do
+            "We'll only contact you about service updates and the chance to "
+            "try out pre-release software"
+
+    footer ! id_ "footer-wrapper" $ do
+      div ! id_ "footer" $ do
+        div ! id_ "links" $ do
+          a ! href "https://medium.com/@concertdaw" $ "Blog"
+          a ! href "https://twitter.com/@concertdaw" $ "Twitter"
+          a ! href "https://github.com/concert" $ "Open" >> nbsp >> "Source"
+        div ! id_ "copyright" $ do
+          "Copyright "
+          copy
+          " 2017 "
+          a ! href "/company" $ do
+            intersperseM
+               nbsp ["Concert", "Audio", "Technologies", "Limited"]
+
+nbsp :: MarkupM ()
+nbsp = preEscapedToHtml ("&nbsp;" :: Text)
+
+copy :: MarkupM ()
+copy = preEscapedToHtml ("&copy;" :: Text)
 
 emailSubmissionConfirmation :: Text -> Html
 emailSubmissionConfirmation email = docTypeHtml $ do
@@ -49,3 +108,11 @@ emailUnsubscriptionConfirmation = docTypeHtml $ do
 
 pretty404 :: Html
 pretty404 = undefined
+
+picture :: Markup -> Markup
+picture = customParent "picture"
+
+intersperseM :: (Monad m) => m a -> [m a] -> m ()
+intersperseM _ [] = return ()
+intersperseM _ (m:[]) = void m
+intersperseM m1 (m2:ms) = m2 >> m1 >> intersperseM m1 ms
