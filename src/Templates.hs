d4a7e96e3ab4a0d4d05a360a6eaf4be2ef333a91
Paul Weaver <paul@concertdaw.co.uk>
2017-10-23 09:40:23 +0100

    Abstract out page skeleton

--- src/Templates.hs
+++ src/Templates.hs
@@ -4,7 +4,9 @@ module Templates where
 
 import Prelude hiding (div)
 import Control.Monad
+import Data.Monoid
 import Data.Text (Text)
+import qualified Data.Text as Text
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Internal (customParent, MarkupM)
@@ -16,60 +18,31 @@ id_ = A.id
 mainSiteLink = "https://www.concertdaw.co.uk"
 
 emailSubmission :: Html
-emailSubmission = docTypeHtml $ do
-  H.head $ do
-    meta ! name "viewport" ! content "width=device-width, initial-scale=1"
-    H.title "Register Interest - Concert"
-    link ! rel "stylesheet" ! href "/screen.css"
-  body $ do
-    header ! id_ "header-wrapper" $ do
-      div ! id_ "header" $ do
-        a ! href mainSiteLink $ do
-          picture $ do
-            source
-            img
-        a ! href "/about" $ "About Us"
-
-    div ! id_ "content-wrapper" $ do
-      div ! id_ "content" $ do
-        div ! id_ "description" $ do
-          h1 $ do
-            "Collaborative Audio Production"
-          p $ do
-            "We're building tools to help you work on audio projects together "
-            "across the internet."
-          p $ do
-            "Our software is "
-            a ! href "https://github.com/concert" $ do
-              "open source"
-            " wherever possible so that you are free to use it however you "
-            "like and you'll always be able to access your data."
-          p $ do
-            "Sign up to our pre-release mailing list to register interest in "
-            "beta testing."
-        H.form ! method "post" ! id_ "registration-form" $ do
-          H.label ! for "email" $ do
-            "Email"
-          input ! id_ "email" ! type_ "text" ! placeholder "name@example.com"
-            ! autofocus "true" -- FIXME: is this right?
-          input ! type_ "submit" ! value "Register interest"
-          aside $ do
-            "We'll only contact you about service updates and the chance to "
-            "try out pre-release software"
-
-    footer ! id_ "footer-wrapper" $ do
-      div ! id_ "footer" $ do
-        div ! id_ "links" $ do
-          a ! href "https://medium.com/@concertdaw" $ "Blog"
-          a ! href "https://twitter.com/@concertdaw" $ "Twitter"
-          a ! href "https://github.com/concert" $ "Open" >> nbsp >> "Source"
-        div ! id_ "copyright" $ do
-          "Copyright "
-          copy
-          " 2017 "
-          a ! href "/company" $ do
-            intersperseM
-               nbsp ["Concert", "Audio", "Technologies", "Limited"]
+emailSubmission = page "Register Interest" $ do
+    div ! id_ "description" $ do
+      h1 $ do
+        "Collaborative Audio Production"
+      p $ do
+        "We're building tools to help you work on audio projects together "
+        "across the internet."
+      p $ do
+        "Our software is "
+        a ! href "https://github.com/concert" $ do
+          "open source"
+        " wherever possible so that you are free to use it however you "
+        "like and you'll always be able to access your data."
+      p $ do
+        "Sign up to our pre-release mailing list to register interest in "
+        "beta testing."
+    H.form ! method "post" ! id_ "registration-form" $ do
+      H.label ! for "email" $ do
+        "Email"
+      input ! id_ "email" ! type_ "text" ! placeholder "name@example.com"
+        ! autofocus "true" -- FIXME: is this right?
+      input ! type_ "submit" ! value "Register interest"
+      aside $ do
+        "We'll only contact you about service updates and the chance to "
+        "try out pre-release software"
 
 nbsp :: MarkupM ()
 nbsp = preEscapedToHtml ("&nbsp;" :: Text)
@@ -78,10 +51,7 @@ copy :: MarkupM ()
 copy = preEscapedToHtml ("&copy;" :: Text)
 
 emailSubmissionConfirmation :: Text -> Html
-emailSubmissionConfirmation email = docTypeHtml $ do
-  H.head $ do
-    H.title "Concert"
-  body $ do
+emailSubmissionConfirmation email = page "Verification Sent" $ do
     h1 "Concert"
     p $ do
       "We sent a verification link to "
@@ -89,22 +59,61 @@ emailSubmissionConfirmation email = docTypeHtml $ do
       ". Please check your inbox."
 
 emailVerificationConfirmation :: Html
-emailVerificationConfirmation = docTypeHtml $ do
-  H.head $ do
-    H.title "Concert"
-  body $ do
+emailVerificationConfirmation = page "Email Verified" $ do
     h1 "Concert"
     p $ do
       "Thank you for verifying your email address. We'll email you when we've "
       "got news about our software or when we're looking for beta testers."
 
 emailUnsubscriptionConfirmation :: Html
-emailUnsubscriptionConfirmation = docTypeHtml $ do
-  H.head $ do
-    H.title "Concert"
-  body $ do
+emailUnsubscriptionConfirmation = page "Unsubscribed" $ do
     h1 "Concert"
     p "We've removed your email address from our list. Sorry to see you go."
+
+
+page :: Text -> Html -> Html
+page pageTitle pageContent = docTypeHtml $ do
+    htmlHead
+    body $ do
+      pageHeader
+      contentWrapper
+      pageFooter
+  where
+    titleText = text $
+        if Text.null pageTitle then "Concert" else pageTitle <> " - Concert"
+    htmlHead = H.head $ do
+        meta ! name "viewport" ! content "width=device-width, initial-scale=1"
+        H.title titleText
+        link ! rel "stylesheet" ! href "/screen.css"
+
+    pageHeader =
+        header ! id_ "header-wrapper" $ do
+          div ! id_ "header" $ do
+            a ! href mainSiteLink $ do
+              picture $ do
+                source
+                img
+            a ! href "/about" $ "About Us"
+
+    contentWrapper =
+        div ! id_ "content-wrapper" $ do
+          div ! id_ "content" $ do
+            pageContent
+
+    pageFooter =
+        footer ! id_ "footer-wrapper" $ do
+          div ! id_ "footer" $ do
+            div ! id_ "links" $ do
+              a ! href "https://medium.com/@concertdaw" $ "Blog"
+              a ! href "https://twitter.com/@concertdaw" $ "Twitter"
+              a ! href "https://github.com/concert" $ "Open" >> nbsp >> "Source"
+            div ! id_ "copyright" $ do
+              "Copyright "
+              copy
+              " 2017 "
+              a ! href "/company" $ do
+                intersperseM
+                   nbsp ["Concert", "Audio", "Technologies", "Limited"]
 
 pretty404 :: Html
 pretty404 = undefined
